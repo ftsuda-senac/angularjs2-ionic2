c11126b0e5455a2bc846f89ee2037f933cdb4784
ftsuda82 <fernando@tvod.com.br>
2016-10-03 21:11:47 -0300

    Inclusao do service mock

---
+++ app-angular2/app/produto-mock.ts
@@ -0,0 +1,11 @@
+import { Produto } from './produto';
+
+export var PRODUTOS: Produto[] = [
+	{'id': 1, 'nome': 'Bolo 1', 'descricao': 'xpto 111', 'preco': 1.11 },
+	{'id': 2, 'nome': 'Bolo 2', 'descricao': 'xpto 222', 'preco': 2.22 },
+	{'id': 3, 'nome': 'Bolo 3', 'descricao': 'xpto 333', 'preco': 3.33 },
+	{'id': 4, 'nome': 'Bolo 4', 'descricao': 'xpto 444', 'preco': 4.44 },
+	{'id': 5, 'nome': 'Bolo 5', 'descricao': 'xpto 555', 'preco': 55.55 },
+	{'id': 6, 'nome': 'Bolo 6', 'descricao': 'xpto 666', 'preco': 66.66 }
+
+]; 
---
+++ app-angular2/app/produto.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Produto } from './produto';
+import { PRODUTOS } from './produto-mock';
+
+@Injectable()
+export class ProdutoService {
+
+	listar(): Promise<Produto[]> {
+		return Promise.resolve(PRODUTOS);
+	}
+
+	obter(id: number): Promise<Produto> {
+		return Promise.resolve(PRODUTOS).then(
+			// *** USANDO SINTAXE "ARROW FUNCTION DO ES2015 ***
+			//resultados => resultados.filter(prod => prod.id === id)[0]
+	
+			// Funcao abaixo eh equivalente à acima.
+			function(resultados) {
+				return resultados.filter(function(prod) {
+					return prod.id === id;
+				})[0];
+			}
+		);
+	}
+	
+	incluir(produto: Produto) {
+		Promise.resolve(PRODUTOS).then(
+			function(resultados) {
+				let ultimo = resultados.reduce(function(prev, current) {
+					return (prev.id > current.id) ? prev : current;
+				});
+				let proximoId = ultimo.id + 1;
+				produto.id = proximoId;
+				resultados.push(produto);
+			}
+		)
+	}
+
+}
